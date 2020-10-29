69b1fb5a7bde527476c47b253c568c38bb1f5693
István Rózsa <rozsa.istvan@encosoft.hu>
2020-10-29 22:28:11 +0100

    dev(Controllers): add swagger

--- ProjectManagementWebApp/ProjectManagementWebApp/Startup.cs
+++ ProjectManagementWebApp/ProjectManagementWebApp/Startup.cs
@@ -22,12 +22,15 @@ namespace ProjectManagementWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddControllers();
 
             services.AddDbContext<ProjectManagementContext>(options =>
                     //options.UseSqlServer(Configuration.GetConnectionString("ProjectManagementContext")));
                     options.UseInMemoryDatabase("ProjectManagementWebApp"));
 
             services.ConfigureServices();
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +39,16 @@ namespace ProjectManagementWebApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                // Enable middleware to serve generated Swagger as a JSON endpoint.
+                app.UseSwagger();
+
+                // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+                // specifying the Swagger JSON endpoint.
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                });
             }
             else
             {
