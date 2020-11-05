ea61b06eb9474039d26c6c9e2ef129c938518f94
Cserkúti Péter <cserkuti.peter@encosoft-dev.hu>
2020-11-05 08:07:24 +0100

    Initial state for Lab 9 added

---
+++ Lab 9 - Start/ProjectManagementWebApp/ProjectManagementWebApp/Models/DbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using ProjectManagementWebApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagementWebApp.Models
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(ProjectManagementContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Project.Any())
+            {
+                return;
+            }
+
+            var projects = new Project[]
+            {
+                new Project  { Name = "Project #1", Description = "Description #1"},
+                new Project  { Name = "Project #2", Description = "Description #2"},
+                new Project  { Name = "Project #3", Description = "Description #3"},
+                new Project  { Name = "Project #4", Description = "Description #4"},
+            };
+
+            context.Project.AddRange(projects);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Lab 9 - Start/ProjectManagementWebApp/ProjectManagementWebApp/Pages/Projects/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagementWebApp.Data;
+using ProjectManagementWebApp.Models;
+
+namespace ProjectManagementWebApp.Pages.Projects
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ProjectManagementWebApp.Data.ProjectManagementContext _context;
+
+        public IndexModel(ProjectManagementWebApp.Data.ProjectManagementContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Project> Project { get;set; }
+
+        public async Task OnGetAsync(string SearchString)
+        {
+            Project = await _context.Project.ToListAsync();
+        }
+    }
+}
